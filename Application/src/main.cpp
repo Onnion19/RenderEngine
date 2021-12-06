1a4a6a5252a6d7a8ff9c4add5f0d9d76b382ef77
Onnion19 <aleixrius19@gmail.com>
2021-12-06 19:53:41 +0100

    Update sample

--- Application/src/main.cpp
+++ Application/src/main.cpp
@@ -4,12 +4,13 @@
 #include <iostream>
 #include <functional>
 #include "Window/Window.h"
-#include "Core/Renderer.h"
-
+#include "Input/InputManager.h"
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+
+bool close = false;
 
 int main()
 {
@@ -26,7 +27,24 @@ int main()
 		return -1;
 	}
 
-	int i; 
-	std::cin >> i;
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	auto lambda = [](Renderer::Input::KeyInfo key) {
+		std::cout << static_cast<int32>(key.code) << std::endl;
+	};
+
+	auto token = context.GetInputManager()->RegisterEvent(lambda, Renderer::Input::KeyboardCode::A, Renderer::Input::ButtonStatus::DOWN);
+
+	while (!close)
+	{
+		context.PullInputEvents();
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+			close = true;
+		glfwPollEvents();
+		glfwSwapBuffers(window.get());
+	}
 	return 0;
 }
